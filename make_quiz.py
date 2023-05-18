6fd25ea2742e29da8a30f0f3f8581e2f0d2244e6
Kaseke <kasymkhankhubievnis@gmail.com>
2023-05-18 15:16:37 +0700

    commit changes

--- make_quiz.py
+++ make_quiz.py
@@ -1,8 +1,65 @@
 import json
+from aiogram import Bot
+from aiogram.types import Message, ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+import os
+
+from tokens import token
 
 
-def main():
-    pass
+bot = Bot(token=token)
+dp = Dispatcher(bot)
 
-if __name__=='__main__':
-    main()
+async def on_startup(_):
+    print('Bot is online')
+
+############ client
+
+b1 = KeyboardButton('/тест')
+b2 = KeyboardButton('/цитатка')
+b3 = KeyboardButton('/интересное')
+b4 = KeyboardButton('/помощь')
+
+main_kb = ReplyKeyboardMarkup()
+main_kb.add(b1).add(b2).add(b3).add(b4)
+
+@dp.message_handler(commands=['start', 'help', 'начать', 'помощь'])
+async def command_start(message: Message):
+    await bot.send_message(message.from_user.id, 'Привет! Я бот Батя-Пальчик, выучим с тобой билеты по ФОИ!', reply_markup=main_kb)
+
+
+@dp.message_handler(commands=['test', 'тест'])
+async def command_start_test(message: Message):
+    await bot.send_message(message.from_user.id, 'Начнем тест!')
+
+
+@dp.message_handler(commands=['citation', 'цитатка'])
+async def command_citation(message: Message):
+    await message.answer('Тут будет цитатка')
+
+
+@dp.message_handler(commands=['intersting', 'интересное'])
+async def command_intersting(message: Message):
+    await message.answer('Что-нибудь интересненькое')
+
+
+############ admin
+
+
+############ basic
+
+@dp.message_handler()
+async def echo_sampler(message: Message):
+    await message.answer(message.text)
+
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    # await message.reply(message.text)
+
+
+# def main():
+#     pass
+
+# if __name__=='__main__':
+#     main()
---
+++ random_question_sampler.py
@@ -0,0 +1,97 @@
+import json
+import io
+from typing import NamedTuple
+import random
+
+quetions_json_file = 'questions.json'
+
+class Question(NamedTuple):
+    question: str
+    answer1: str
+    answer2: str
+    answer3: str
+    answer4: str
+    true_answer: str
+
+
+def load_questions() -> dict:
+    """
+    loads json file with questions
+    """
+    with io.open(quetions_json_file, encoding='utf-8-sig', mode='r') as json_file:
+        questions_data = json.load(json_file)
+
+    return questions_data
+
+
+def pack_questions(questions_data: dict) -> 'dict[int, Question]':
+    """
+    {'номер вопроса':{
+        'Вопрос': 'str',
+        'Варианты ответов': {
+            '1': 'str',
+            '2': 'str',
+            '3': 'str',
+            '4': 'str'
+            },
+        'Верный ответ': 'str',
+        }
+    }
+    """
+    questions_dict = dict()
+    keys = questions_data.keys()
+    for key in keys:
+        sub_dict = questions_data[key]
+        questions_dict[int(key)] = Question(question=sub_dict['Вопрос'], 
+                                       answer1=sub_dict['Варианты ответов']['1'],
+                                       answer2=sub_dict['Варианты ответов']['2'],
+                                       answer3=sub_dict['Варианты ответов']['3'],
+                                       answer4=sub_dict['Варианты ответов']['4'],
+                                       true_answer=sub_dict['Верный ответ'])
+        
+    return questions_dict
+
+
+def main():
+    runable = True
+    score = 0
+    total = 0
+
+    questions = pack_questions(load_questions())
+    questions_num = len(questions.keys())
+
+    while runable:
+        question_id = random.randint(1, questions_num)
+        message = f'Вопрос: {questions[question_id].question}\n' +\
+                  f'1)  {questions[question_id].answer1}\n' +\
+                  f'2)  {questions[question_id].answer2}\n' +\
+                  f'3)  {questions[question_id].answer3}\n' +\
+                  f'4)  {questions[question_id].answer4}\n' + 'Введите номер ответа\n для выхода введите "exit".\n'
+        answer = input(message)
+
+        if answer.lower() == 'exit':
+            runable = False
+            print(f'\n\nYour score is {score}/{total}')
+            break
+        
+        if answer.isdigit():
+            if questions[question_id].true_answer[0] == answer:
+                score += 1
+                total += 1
+                print('Верный ответ!')
+                print(questions[question_id].true_answer + '\n\n')
+            else:
+                total += 1
+                print('Неверный ответ!')
+                print(questions[question_id].true_answer + '\n\n')
+        else:
+            print('нужно было ввести число')
+            # print('Нужно ввести номер ответа - число! \n\n')
+            # print(message)
+
+    
+
+
+
+if __name__=='__main__':
+    main()
